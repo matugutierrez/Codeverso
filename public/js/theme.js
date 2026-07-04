669e34acdd290921a412c21399677ebc7e0d6a3a
Matu gutierrez <matugutierrez444@gmail.com>
2026-07-04 05:11:12 -0300

    fixes

--- public/js/theme.js
+++ public/js/theme.js
@@ -4,10 +4,8 @@
   function setTheme(theme) {
     if (theme === 'dark') {
       html.setAttribute('data-theme', 'dark')
-      if (btn) btn.textContent = '\uD83D\uDCA1'
     } else {
       html.removeAttribute('data-theme')
-      if (btn) btn.textContent = '\uD83C\uDF19'
     }
     try { localStorage.setItem('codeverso-theme', theme) } catch (e) {}
   }
